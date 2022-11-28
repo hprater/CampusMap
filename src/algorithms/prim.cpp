0345f51735c4a2a22fc9779433d2e9ccf56cb74d
Hayden Prater <Hayden33prater@outlook.com>
2022-11-28 13:43:29 -0600

    Updated version

---
+++ src/algorithms/prim.cpp
@@ -0,0 +1,26 @@
+#include <graph.hpp> 
+#include <sort.hpp> 
+#include <disjoint_set.hpp>
+#include <queue>
+
+struct EdgeKeyComparison {
+    constexpr bool operator()(const Edge &a, const Edge &b) const noexcept {
+        return a.w > b.w;
+    }
+};
+
+
+
+std::vector<Edge> constructMSTPrim(Graph G) {
+    std::vector<Edge> edges = G.exportEdges(); // Graph's edges
+
+    // std::priority_queue< Edge, std::vector<Edge>, EdgeKeyComparison > heap;
+    // If you want to use heap to optimize the minimum searching, you can use heap defined as above
+    //      Insert: heap.push(Edge(u, -1, distance));
+    //      Get Minimum: top = heap.top(); u = top.u; distance = top.w;
+    //      Remove top: heap.pop(); (this function usually goes after the get minimum method)
+    
+    // YOUR CODE HERE
+
+}
+
