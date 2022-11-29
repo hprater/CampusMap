3561135d0ec56cb2e36f34002aa2914426e762c6
Hayden Prater <Hayden33prater@outlook.com>
2022-11-29 11:41:29 -0600

    Done

--- src/algorithms/dijkstra.cpp
+++ src/algorithms/dijkstra.cpp
@@ -11,12 +11,43 @@ struct EdgeKeyComparison {
 };
 
 std::vector<int> searchShortestPath(Graph &G, int start, int destination) {
-    
-    // std::priority_queue< Edge, std::vector<Edge>, EdgeKeyComparison > heap;
-    // If you want to use heap to optimize the minimum searching, you can use heap defined as above
-    //      Insert: heap.push(Edge(u, -1, distance));
-    //      Get Minimum: top = heap.top(); u = top.u; distance = top.w;
-    //      Remove top: heap.pop(); (this function usually goes after the get minimum method)
-    
     // YOUR CODE HERE
+    std::vector<int> distance(G.n, INT_MAX);
+    std::vector<int> parent(G.n, -1);
+
+    distance[start] = 0;
+    parent[start] = -1;
+
+    for (int idx = 0; idx < G.n; ++idx) {
+        int min = INT_MAX, u;
+        for (int i = 0; i < G.n; ++i)
+            if (distance[i] < min && !G.isVisited(i)) {
+                min = distance[i];
+                u = i;
+            }
+        G.setVisited(u);
+
+        if (u == destination)
+            break;
+
+        for (int i = 0; i < G.e[u].size(); ++i) {
+            int v = G.e[u][i].v;
+            int w = G.e[u][i].w;
+            if(G.isVisited(v))
+                continue;
+            if (distance[u] + w < distance[v]) {
+                distance[v] = w + distance[u];
+                parent[v] = u;
+            }
+        }
+    }
+
+    std::vector<int> path;
+    int u = destination;
+    while (u != -1) {
+        path.push_back(u);
+        u = parent[u];
+    }
+    std::reverse(path.begin(), path.end());
+    return path;
 }
--- src/algorithms/kruskal.cpp
+++ src/algorithms/kruskal.cpp
@@ -5,12 +5,6 @@
 #include <sort.hpp> 
 #include <disjoint_set.hpp>
 
-int find(int u, std::vector<int> &parent) {
-    if (parent[u] == -1)
-        return u;
-    else
-        return parent[u] = find(parent[u], parent);
-}
 
 std::vector<Edge> constructMSTKruskal(Graph G) {
     std::vector<Edge> edges = G.exportEdges(); // Graph's edges
@@ -24,14 +18,16 @@ std::vector<Edge> constructMSTKruskal(Graph G) {
 
     std::vector<int> parent(n, -1);
     sort(edges, 0, edges.size()-1);
+
     std::vector<Edge> MST;
+    DisjointSet djs (G.n);
+
     for (int i = 0; i < edges.size(); ++i) {
         int u = edges[i].u;
         int v = edges[i].v;
-        int pu = find(u, parent);
-        int pv = find(v, parent);
-        if (pu != pv) {
-            parent[pu] = pv;
+        int value = djs.isOnSameSet(u, v);
+        if (value != 1){
+            djs.join(u,v);
             MST.push_back(Edge(edges[i]));
         }
     }
